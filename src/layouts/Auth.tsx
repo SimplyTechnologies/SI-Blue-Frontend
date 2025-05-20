78728301b3bbc86f6957f4820cac064f91fcd952
Hayk-Darbinyan <haykdarbinyan04@gmail.com>
2025-05-20 15:11:43 +0400

    refactor: validations for auth inputs

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -1,14 +1,10 @@
 import { Outlet } from 'react-router';
 import carBg from '@/assets/carBackground.svg';
-import carlifeLogo from '@/assets/carlifeLogo.svg';
 
 const Auth: React.FC = () => {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-start gap-2 md:justify-start">
-          <img src={carlifeLogo} alt="carlifeLogo" />
-        </div>
+      <div className="flex flex-col gap-4 ">
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-[450px]">
             <Outlet />
