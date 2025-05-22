1d3df80bb3ef6fba0be06a0e36c67322bbb1d008
Hayk-Darbinyan <haykdarbinyan04@gmail.com>
2025-05-22 10:14:31 +0400

    feat: vehicles page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -10,17 +10,15 @@ const Dashboard: React.FC = () => {
           + Add New Vehicle
         </Button>
       </div>
-          <p className="text-[var(--color-support-6)] text-[length:var(--sm-text)] font-[var(--fw-bold)] leading-[140%]">
-            Total analytics
-          </p>
+      <p className="text-[var(--color-support-6)] text-[length:var(--sm-text)] font-[var(--fw-bold)] leading-[140%]">
+        Total analytics
+      </p>
       <div className="flex flex-wrap justify-evenly items-end gap-[1rem]">
-        {/* <div className="flex-1 w-[313.5px] max-[480px]:w-full flex flex-col gap-[1.5rem]"> */}
-          <AnalyticCard title={'vehicle'} count={3656} />
-        {/* </div> */}
-        <AnalyticCard title={'customers'} count={3656} />
-        <AnalyticCard title={'vehiclesSold'} count={1276} />
+        <AnalyticCard title={'vehicle'} count={0} />
+        <AnalyticCard title={'customers'} count={0} />
+        <AnalyticCard title={'vehiclesSold'} count={0} />
       </div>
-      <div className='h-full max-h-[608px] max-[1024px]:min-h-[200px] max-[480px]:min-h-[400px] mt-[2rem]'>
+      <div className="h-full max-h-[608px] max-[1024px]:min-h-[200px] max-[480px]:min-h-[400px] mt-[2rem]">
         <Map />
       </div>
     </div>
