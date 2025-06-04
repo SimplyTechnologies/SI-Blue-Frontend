67dd1937c15cb3fa1eeef34cb246fd8c49ab4230
nika_jaghinyan <nika.jaghinyan@gmail.com>
2025-06-04 11:09:37 +0400

    chore: add date validation

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -4,7 +4,8 @@ export const isObjectEmpty = (obj: Record<string, unknown>) => {
 
 export const formatDate = (dateString: string) => {
   const date = new Date(dateString);
-  
+  if (isNaN(date.getTime())) return;
+
   const dd = String(date.getDate()).padStart(2, '0');
   const mm = String(date.getMonth() + 1).padStart(2, '0'); // January is 0!
   const yyyy = date.getFullYear();
