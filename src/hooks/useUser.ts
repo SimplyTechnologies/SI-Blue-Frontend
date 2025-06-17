e5ef65943b0e135bd4a046ed92a84b526eb8ad19
haykgyulabyan <gyulabyanh@gmail.com>
2025-06-17 15:01:42 +0400

    feature: allow user to upload/delete avatar image

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -3,6 +3,8 @@ import { toast } from 'sonner';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import api from '@/api/axios';
+import { updateUserAvatar, deleteUserAvatar } from '@/api/user';
+import type { UpdateAvatarRequest, UpdateAvatarResponse } from '@/types/User';
 
 export const useDeleteUser = () => {
   const queryClient = useQueryClient();
@@ -20,6 +22,38 @@ export const useDeleteUser = () => {
       const axiosError = error as AxiosError<{ message: string }>;
       const message = axiosError.response?.data?.message || axiosError.message || 'Failed to delete user';
       toast.error(message);
+    },
+  });
+};
+
+export const useUploadAvatar = () => {
+  return useMutation<UpdateAvatarResponse, Error, UpdateAvatarRequest>({
+    mutationFn: async (data: UpdateAvatarRequest) => {
+      return  await updateUserAvatar(data.id, data.body);
+    },
+    onSuccess: () => {
+      toast.success('Avatar uploaded successfully');
+    },
+    onError: error => {
+      const axiosError = error as AxiosError<{ message: string }>;
+      const message = axiosError.response?.data?.message || axiosError.message;
+      toast.error(message ?? 'Failed to upload avatar. Please try again.');
+    },
+  });
+};
+
+export const useDeleteAvatar = () => {
+  return useMutation<void, Error, number>({
+    mutationFn: async (id: number) => {
+      return deleteUserAvatar(id);
+    },
+    onSuccess: () => {
+      toast.success('Avatar deleted successfully');
+    },
+    onError: error => {
+      const axiosError = error as AxiosError<{ message: string }>;
+      const message = axiosError.response?.data?.message || axiosError.message;
+      toast.error(message ?? 'Failed to delete avatar. Please try again.');
     },
   });
 };
